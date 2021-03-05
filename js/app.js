88c7938b8e7e07824acfc7d4ac2221d6a9932684
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2021-03-05 14:37:21 -0600

    2 sections updated

--- js/app.js
+++ js/app.js
@@ -8,3 +8,21 @@ const navSlide = () => {
 }
 
 navSlide();
+
+axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+.then(response => {
+var data = response.data.categories;
+console.log(data);
+for (var i = 0; i < data.length; i++) {
+  var foodType = data[i].strCategory;
+  var thumbnail = data[i].strCategoryThumb;
+  var description = data[i].strCategoryDescription;
+  }
+  console.log(foodType, thumbnail, description);
+  var cardImage = document.getElementById("#card-image");
+  cardImage.onload(function() {
+    
+  })
+}
+)
+.catch(error => console.log(error));
